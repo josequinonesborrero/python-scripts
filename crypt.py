c414397a4fd4cf5652f54bd5614e2fccc5383661
Jose Quinones <josequinones@codefidelio.org>
2013-07-21 08:06:23 -0400

    alpha version 0.2

--- crypt.py
+++ crypt.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+# This script is ment to be used as a single source to process strings and files
+# using diffrents types of hashing, encoding and ecrypting algorythms.
+# Coded by: Jose L Quinones (python n00b)
+# Version: 0.2
+# Changes: Added rot13 and hex support
+# Pending: Add encryption and cracking capabilities, fix file processing
+
 import hashlib, base64, sys
 
 hashes = ['md5','sha1','sha256','sha512']
@@ -9,53 +16,55 @@ content = ''
 
 def print_usage():
     print ''
-    print 'Usage: crypt.py [options] [argument]'
+    print 'Usage: crypt.py [operationtype] [argument] [sourcetype] [argument] [extras]'
     print ''
-    print ' --hash      : hash [md5, sha1, sha256, sha512]'
-    print ' --enc       : encoding [base64, rot13]'
-    print ' --dec       : encoding [base64, rot13]'
-    print ' --string    : string'
-    print ' --file      : file'
-    print ' --help      : print this help'
+    print ' --hash      	: hash [md5, sha1, sha256, sha512]'
+    print ' --encode    	: encoding [base64, hex, rot13]'
+    print ' --decode    	: encoding [base64, hex, rot13]'
+    # print ' --crypt	: encryption [aes256]'
+    # print ' --decrypt	: encryption [aes256]'
+    # print ' --crack	: encryption or hashing [md5, sha1,sha256, sha512, aes256]'
+    print ' --string    	: "string"'
+    print ' --file      	: file'
+    print ' --help      	: print this help'
     print ''
     print '[argument] = string or file to be hashed'
     print ''
     print 'Example:  crypt.py --hash md5 --string my_password'
-    print 'Example:  crypt.py --enc base64 --string secret'
+    print 'Example:  crypt.py --encode base64 --string secret'
+    print 'Example:  crypt.py --crypt aes256 --string secret --key password'
     sys.exit(1)
     
-def check_file():
+def import_f():
     try:
-        f = open(str(sys.argv[4]),  'r')
+        f = open(str(sys.argv[4]),  'rb')
     except IOError:
         print '[!] The file does not exist or was unable to open.'
         print ''
         sys.exit(1)
-    f.close()
-    
-def import_f():
-    f = open(str(sys.argv[4]),'rb')
     content = f.readlines()
     f.close()
-    
+    print content
+    print '[i] file loaded sucessfully'
+
 def md5_hash():
-    hash_string = hashlib.md5(clear_string).hexdigest()
-    print 'md5 hash:'+ hash_string
+    hash_string = hashlib.md5(txt_string).hexdigest()
+    print 'md5 hash: '+ hash_string
 
 def sha1_hash():
-    hash_string = hashlib.sha1(clear_string).hexdigest()
-    print 'sha1 hash:'+ hash_string
+    hash_string = hashlib.sha1(txt_string).hexdigest()
+    print 'sha1 hash: '+ hash_string
 
 def sha256_hash():
-    hash_string = hashlib.sha256(clear_string).hexdigest()
-    print 'sha256 hash:'+ hash_string
+    hash_string = hashlib.sha256(txt_string).hexdigest()
+    print 'sha256 hash: '+ hash_string
 
 def sha512_hash():
-    hash_string = hashlib.sha512(clear_string).hexdigest()
-    print 'sha512 hash:'+ hash_string
+    hash_string = hashlib.sha512(txt_string).hexdigest()
+    print 'sha512 hash: '+ hash_string
     
 def base64_encode():
-    encoded = base64.b64encode(clear_string)
+    encoded = base64.b64encode(txt_string)
     print '------- Start of Base64 encoded text below -------'
     print encoded
     print '-------- End of Base64 encoded text above --------'
@@ -64,8 +73,32 @@ def base64_decode():
     decoded = base64.decodestring(encoded_string)
     print '------- Decoded text below -------'
     print decoded
-
-def hashing_funcs():
+    print '-------- End of Base64 encoded text above --------'
+	
+def ascii2hex():
+    encoded =  txt_string.encode("hex")
+    print '------ Start of hex encoded string below ------ '
+    print encoded
+    print '-------- End of hex encoded text above --------'
+	
+def hex2ascii():
+    decoded = encoded_string.decode("hex")
+    print '------ Start of hex decoded string below ------ '
+    print decoded
+    print '-------- End of hex encoded text above --------'
+	
+def rot13_encode():
+    encoded =  txt_string.encode("rot13")
+    print '------ Start of rot13 encoded string below ------ '
+    print encoded
+    print '-------- End of rot13 encoded text above --------'
+	
+def rot13_decode():
+    decoded = encoded_string.decode("rot13")
+    print '------ Start of rot13 decoded string below ------ '
+    print decoded
+	
+def hashing_funcs():					# cycle truh hashing algorythms and hash
         if str(sys.argv[2]) == 'md5':
             md5_hash()
         if str(sys.argv[2]) == 'sha1':
@@ -75,39 +108,58 @@ def hashing_funcs():
         if str(sys.argv[2]) == 'sha512':
             sha512_hash()
 
-if len(sys.argv) < 2:                   # If no arguments are sent
+def encoding_funcs():					# cycle truh encoding options and encode
+        if str(sys.argv[2]) == 'base64':
+            base64_encode()
+        if str(sys.argv[2]) == 'hex':
+            ascii2hex()
+        if str(sys.argv[2]) == 'rot13':
+            rot13_encode()
+
+if len(sys.argv) < 2:                                   # If no arguments are sent
     print_usage()
     sys.exit(1)
 if str(sys.argv[1]) == '--help':
     print_usage()
     sys.exit(1)
     
-if str(sys.argv[1]) == '--hash':            # check if its a hash function
+if str(sys.argv[1]) == '--hash':                        # check if its a hash function
     if str(sys.argv[3]) == '--string':
-        clear_string = str(sys.argv[4])
+        txt_string = str(sys.argv[4])
         hashing_funcs()
     if str(sys.argv[3]) == '--file':
-        check_file()
         import_f()
-        clear_string = content
+        txt_string = content
         hashing_funcs()
       
-if str(sys.argv[1]) == '--enc':
+if str(sys.argv[1]) == '--encode':			# start of encoding routine
+    txt_string = str(sys.argv[4])
     if str(sys.argv[3]) == '--string':
         if str(sys.argv[2]) == 'base64':
-            clear_string = str(sys.argv[4])
             base64_encode()
+        if str(sys.argv[2]) == 'hex':
+            ascii2hex()
+        if str(sys.argv[2]) == 'rot13':
+            rot13_encode()
     if str(sys.argv[3]) == '--file':
-        check_file()
         import_f()
-        clear_string = content
-        base64_encode()
-        
-if str(sys.argv[1]) == '--dec':
+        txt_string = content
+        if str(sys.argv[2]) == 'base64':
+            base64_decode()
+        if str(sys.argv[2]) == 'hex':
+            hex2ascii()
+        if str(sys.argv[2]) == 'rot13':
+            rot13_decode()
+			
+if str(sys.argv[1]) == '--decode':			# start of decoding strings routine
+    encoded_string = str(sys.argv[4])
     if str(sys.argv[3]) == '--string':
         if str(sys.argv[2]) == 'base64':
-            encoded_string = str(sys.argv[4])
             base64_decode()
+        if str(sys.argv[2]) == 'hex':
+            hex2ascii()
+        if str(sys.argv[2]) == 'rot13':
+            rot13_decode()
 
 else:
     sys.exit(1)
